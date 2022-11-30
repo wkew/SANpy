f16af2f605530ad4df1d0f230859ebedbfe21fa8
wkew <will.kew@gmail.com>
2022-11-30 11:54:11 -0800

    more stuff

--- sanplot.py
+++ sanplot.py
@@ -9,9 +9,12 @@ Created on Tue Nov 22 20:50:25 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 dataloc = r'D:\Coding\SANpy/'
 arr = np.load(dataloc+'test.npy')
+
+
 
 # simple plot
 fig,ax = plt.subplots()
@@ -37,4 +40,71 @@ ax.plot(logx,logy,c='k')
 ax2 = ax.twinx()
 ax2.plot(logx,dy,c='r')
 
+
+
+y,x = np.histogram(logy,bins=1000)
+x = x[:-1]
+
+from scipy.stats import rayleigh
+
+fit = rayleigh.fit(logy)
+
+
+fig,ax = plt.subplots()
+ax.plot(x,y)
+ax.axvline(np.log10(100_000),c='k',ls='--')
+ax.axvline(np.log10(624_145),c='k',ls='--')
+ax.axvline(np.log10(1_200_000),c='k',ls='--')
+
+ax.axvline(fit[0]-1/3*(fit[1]),c='r',ls='--')
+ax.axvline(fit[0]+5/3*(fit[1]),c='r',ls='--')
+ax.axvline(fit[0]+7/3*(fit[1]),c='r',ls='--')
+
+
+noise_thresh = 10**(fit[0]+7/3*(fit[1]))
+
+red_arr = arr[arr<noise_thresh]
+np.median(red_arr)
+np.std(red_arr)*6
+
+
+10**(fit[0]+fit[1])
+
+10**fit[1]
+
+'''
+dist = np.random.rayleigh(fit[1],100000)
+dist.sort()
+dist = dist[::-1]
+x = np.arange(len(dist))
+logx = np.log10(x)
+plt.plot(x, dist )
+
+'''
+
+loc = fit[0]
+sca = fit[1]
+
+fig,ax = plt.subplots()
+ax.plot(x,y)
+ax.axvline(sca)
+
+ax.axvline( (loc+np.log(sca)**2 ))
+
+
+ax.axvline(fit[0]+2*(fit[1]**2))
+
+
+plt.plot((x/(sca**2))*np.exp((-x**2)/((2*sca)**2)))
+
+
+from lmfit.models import GaussianModel, SkewedGaussianModel, SkewedVoigtModel
+
+
+mod = SkewedVoigtModel()
+
+pars = mod.guess(y, x=x)
+out = mod.fit(y, pars, x=x)
+
+out.plot()
 
